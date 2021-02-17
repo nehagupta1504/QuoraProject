136c750d1efee2fb1a12c2097869ac59755f6cfb
Neha Gupta <nehagupta150498@gmail.com>
2021-02-17 19:34:50 +0530

    Write functionality added, Project completed

--- Back-end/app.js
+++ Back-end/app.js
@@ -2,22 +2,26 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 const bodyParser = require('body-parser');
-// var fs = require("fs");
+ var fs = require("fs");
+const { json } = require('body-parser');
 // var readStream;
-// var writeStream;
+ var writeStream;
 //to read
 //readStream = fs.createReadStream("./data.txt");
+writeStream = fs.createWriteStream('./data.txt');
  app.use(express.urlencoded({
     extended: true
  }));
 app.use(cors());
 app.post('/', bodyParser.json(), (req,res)=>{
   
-    var data = req.body;
+    var data = JSON.stringify(req.body);
     console.log(req.body);
-    console.log(JSON.stringify(data));
-    res.json({"message":"Ok"})
+    //console.log(JSON.stringify(data));
+    res.json({"message":"Ok"});
+    writeStream.write("Interview Problem: "+ req.body.postTitle+"\n\n"+ "Description: "+req.body.postDescription);
 });
+
 // app.get('/', function(req,res){
 //     res.send('Hello World!!');
 //     console.log('Hii There!!');
